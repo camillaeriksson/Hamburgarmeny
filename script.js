206df8792277f73d697be7afc98131a030139b53
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-10-29 10:52:23 +0100

    Added list on desktop i topnav

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function openBar() {
     let navbar = document.querySelector("header")
+    document.querySelector("header").style.transition = "all 1s";
     if (navbar.style.height === "5rem") {
         navbar.style.height = "100vh"
     }
